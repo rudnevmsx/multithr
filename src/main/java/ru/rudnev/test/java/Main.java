b28dd70586f88ec163160c8b026e08b6180531ee
rudnevmsx <rudnevmsx@gmail.com>
2024-02-05 21:16:16 +0300

    hm done

--- src/main/java/ru/rudnev/test/java/Main.java
+++ src/main/java/ru/rudnev/test/java/Main.java
@@ -1,7 +1,68 @@
 package ru.rudnev.test.java;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+
+        Lock lock = new ReentrantLock();
+        Condition conditionA = lock.newCondition();
+        Condition conditionB = lock.newCondition();
+        Condition conditionC = lock.newCondition();
+
+        executor.submit(() -> {
+            lock.lock();
+            try {
+                for (int i = 0; i < 5; i++) {
+                    System.out.print("A");
+                    conditionB.signal();
+                    conditionA.await();
+                }
+                conditionB.signal();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        });
+
+        executor.submit(() -> {
+            lock.lock();
+            try {
+                for (int i = 0; i < 5; i++) {
+                    System.out.print("B");
+                    conditionC.signal();
+                    conditionB.await();
+                }
+                conditionC.signal();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        });
+
+        executor.submit(() -> {
+            lock.lock();
+            try {
+                for (int i = 0; i < 5; i++) {
+                    System.out.print("C");
+                    conditionA.signal();
+                    conditionC.await();
+                }
+                conditionA.signal();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        });
+
+        executor.shutdown();
     }
 }
